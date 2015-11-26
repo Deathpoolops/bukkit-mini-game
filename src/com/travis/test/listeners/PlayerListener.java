c2639261f8f6c54f6584fdcae5eef6491d953655
Travis Woodworth-Smith <travisws@me.com>
2015-11-26 02:12:07 -0700

    Did a  lot of work. Got the scoreboard working and more

--- src/com/travis/test/listeners/PlayerListener.java
+++ src/com/travis/test/listeners/PlayerListener.java
@@ -1,8 +1,15 @@
 package com.travis.test.listeners;
 
+import com.mojang.authlib.GameProfile;
+import com.travis.test.Main;
 import com.travis.test.teamSetUP.teamManger.TeamSetUp;
 import com.travis.test.utilities.Commands;
+import net.minecraft.server.v1_8_R3.*;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,10 +20,15 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.potion.PotionEffect;
 
+import java.util.UUID;
+
 /**
  * Created by Deathpoolops on 11/7/15.
  */
 public class PlayerListener implements Listener {
+
+    public EntityPlayer npc;
+
 
     @EventHandler
     public void selectKit(PlayerInteractEvent player){
@@ -42,12 +54,24 @@ public class PlayerListener implements Listener {
         }
         //TODO Need to change to a banner on the screen
         player.sendMessage(ChatColor.GREEN + "Hello " + player.getName());
+
+      /*  MinecraftServer server = ((CraftServer) Bukkit.getServer()).getServer();
+        WorldServer worldServer = ((CraftWorld) Bukkit.getServer().getWorlds().get(0)).getHandle();
+        npc = new EntityPlayer(server, worldServer, new GameProfile(UUID.randomUUID(), "NPC"), new PlayerInteractManager(worldServer));
+
+
+        npc.teleportTo(e.getPlayer().getLocation(), false);
+
+        PlayerConnection connection = ((CraftPlayer) e.getPlayer()).getHandle().playerConnection;
+        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, npc));
+        connection.sendPacket(new PacketPlayOutNamedEntitySpawn(npc));*/
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerDeath(PlayerDeathEvent e) {
         //TODO Need to add spawn point on death
         Player player = e.getEntity();
+        Commands.tpr(player);
         player.getInventory().clear();
         Commands.tpr(player);
     }
